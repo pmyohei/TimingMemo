bddfa65a4358a4a124df349c51a085e01c8289ab
pmyohei <t.yohei@kandg.co.jp>
2022-09-08 18:27:18 +0900

    Initial commit

---
+++ app/src/main/java/com/example/timingmemo/MemoListActivity.java
@@ -0,0 +1,16 @@
+package com.example.timingmemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memo_list);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/timingmemo/ui/record/RecordFragment.java
@@ -0,0 +1,28 @@
+package com.example.timingmemo.ui.record;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.timingmemo.R;
+
+public class RecordFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_record, container, false);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
